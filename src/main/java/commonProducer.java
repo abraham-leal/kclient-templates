db75fe451233b226acb36a68fc01a5677402a642
Abraham Leal <aleal@confluent.io>
2020-02-23 21:35:36 -0600

    add boilerplate producer

---
+++ src/main/java/commonProducer.java
@@ -0,0 +1,70 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.log4j.BasicConfigurator;
+
+import java.util.Properties;
+
+public class commonProducer {
+
+    private static final String TOPIC = "TestingTopic";
+
+    public static Properties getConfig (){
+        final Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 5);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "TestingProducer");
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 100);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 500);
+        props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+
+        return props;
+    }
+
+    public static void main(final String[] args) {
+        BasicConfigurator.configure();
+
+        //Start producer with configurations
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(getConfig());
+
+        try {
+
+            // Start a finite loop, you normally will want this to be a break-bound while loop
+            // However, this is a testing loop
+            for (long i = 0; i < 3000; i++) {
+                //Std generation of fake key and value
+                final String orderId = Long.toString(i);
+                final String payment = Integer.toString(orderId.hashCode());
+
+                // Generating record without header
+                final ProducerRecord<String, String> record = new ProducerRecord<String, String>(TOPIC, orderId, payment);
+
+                //Sending records and displaying metadata with a non-blocking callback
+                //This allows to log/action on callbacks without a synchronous request
+                producer.send(record, ((recordMetadata, e) -> {
+                    System.out.println("Record was sent to topic " +
+                            recordMetadata.topic() + " with offset " + recordMetadata.offset() + " in partition " + recordMetadata.partition());
+                }));
+            }
+
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        //Tell producer to flush before exiting
+        producer.flush();
+        System.out.printf("Successfully produced messages to a topic called %s%n", TOPIC);
+
+        //Shutdown hook to assure producer close
+        Runtime.getRuntime().addShutdownHook(new Thread(producer::close));
+
+    }
+
+}
--- src/main/java/statefulStream.java
+++ src/main/java/statefulStream.java
@@ -2,7 +2,13 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
+import org.apache.kafka.streams.state.StoreBuilder;
+import org.apache.kafka.streams.state.Stores;
 import org.apache.log4j.BasicConfigurator;
 
 import java.util.Arrays;
@@ -10,39 +16,62 @@ import java.util.Properties;
 
 public class statefulStream {
 
-    public static void main(String[] args) {
-        BasicConfigurator.configure();
+    private static String TOPIC = "TestingTopic";
+    private static String OUTPUT_TOPIC = "StreamEnd";
+    private static String STORE = "InternalStore";
 
-        final String TOPIC = "TestingTopic";
-        final String outputTopic = "StreamEnd";
 
+    public static Properties getConfig (){
         final Properties streamsProps = new Properties();
         streamsProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "boilerplateStream");
         streamsProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         streamsProps.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         streamsProps.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        streamsProps.put(StreamsConfig.STATE_DIR_CONFIG, "/tmp/state-store");
+
+        return streamsProps;
+    }
+
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        //Build Internal, persistent store, with String Serdes for both key and value, if wanting to use only an in-memory store
+        // look into inMemoryKeyValueStore
+        StoreBuilder<KeyValueStore<String,String>> storedState =
+                Stores.keyValueStoreBuilder(Stores.persistentKeyValueStore(STORE),Serdes.String(),Serdes.String());
+
+        //Attach topology to state store
+        builder.addStateStore(storedState);
+
+        // No need to specify Serdes as builder will get the default set in properties
+        final KStream<String, String> input = builder.stream(TOPIC);
+
+        //Build topology
+        final KStream<String, String> [] evaulateRecord = input.branch(
+                (key, value) -> key.startsWith("1"),
+                (key, value) -> key.startsWith("2")
+        );
+
+        //The count aggregation will utilized the specified store, rather than abstracting one
+        //Otherwise, it would abstract away the store and create two stores
+        final KTable<String,Long> OneKeys = evaulateRecord[0].groupByKey().count();
+        final KTable<String,Long> TwoKeys = evaulateRecord[1].groupByKey().count();
+
+        OneKeys.toStream().to(OUTPUT_TOPIC);
+        TwoKeys.toStream().to(OUTPUT_TOPIC);
+
+        final KafkaStreams streams = new KafkaStreams(builder.build(), getConfig());
 
         try {
-            final StreamsBuilder builder = new StreamsBuilder();
-
-            final KStream<String, String> messages = builder.stream(TOPIC);
-
-            final KStream<String, String> processing = messages.flatMapValues(value -> Arrays.asList(value.split("\\s+")));
-
-            processing.to(outputTopic);
-
-            final KafkaStreams streams = new KafkaStreams(builder.build(), streamsProps);
-
-            streams.cleanUp();
-
             streams.start();
-
-            Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-
         }
         catch (Exception e) {
             e.printStackTrace();
         }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
 }
