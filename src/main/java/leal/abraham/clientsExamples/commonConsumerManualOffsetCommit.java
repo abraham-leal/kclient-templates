3edfa52ec245bdc945fe0f4bd5926253868b13bb
Abraham Leal <aleal@confluent.io>
2020-11-13 10:58:19 -0600

    update

--- src/main/java/leal/abraham/clientsExamples/commonConsumerManualOffsetCommit.java
+++ src/main/java/leal/abraham/clientsExamples/commonConsumerManualOffsetCommit.java
@@ -1,6 +1,8 @@
 package leal.abraham.clientsExamples;
 
+import org.apache.kafka.clients.consumer.CommitFailedException;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -8,9 +10,13 @@ import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.config.SslConfigs;
+import org.apache.kafka.common.errors.AuthorizationException;
+import org.apache.kafka.common.errors.InterruptException;
 import org.apache.kafka.common.serialization.ByteArrayDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
 import java.time.Duration;
 import java.util.Collection;
@@ -21,8 +27,9 @@ import java.util.Properties;
 import java.util.Set;
 
 public class commonConsumerManualOffsetCommit {
+    private static Logger logging = Logger.getRootLogger();
 
-    private static final String TOPIC = "newTopic";
+    private static final String TOPIC = "confluent-audit-log-events";
 
     public static Properties getConfig (){
         final Properties props = new Properties();
@@ -44,16 +51,31 @@ public class commonConsumerManualOffsetCommit {
 
     public static void main(final String[] args) throws InterruptedException {
         BasicConfigurator.configure();
+        logging.setLevel(Level.INFO);
 
         final KafkaConsumer<String, String> consumer = new KafkaConsumer<String,String>(getConfig());
-        consumer.subscribe(Collections.singletonList(TOPIC));
+        Map<TopicPartition, OffsetAndMetadata> myProcessedOffsets = new HashMap<>();
+        consumer.subscribe(Collections.singletonList(TOPIC), new ConsumerRebalanceListener() {
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                logging.info("Revoked partition assignment, clearing offset map");
+                myProcessedOffsets.clear();
+            }
+
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                logging.info("Received partition assignment, populating offset map");
+                for (TopicPartition partition : partitions) {
+                    myProcessedOffsets.put(partition, new OffsetAndMetadata(consumer.position(partition), null));
+                }
+            }
+        });
 
         try {
-
             while (true) {
                 // We are obtaining a set of records from a partition
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
-                Map<TopicPartition, OffsetAndMetadata> myProcessedOffsets = new HashMap<>();
+
                 for (final ConsumerRecord<String, String> record : records) {
                     // We are iterating through each record in the batch we obtained
                     final String key = record.key();
@@ -64,11 +86,24 @@ public class commonConsumerManualOffsetCommit {
                     // Here, we "finished processing" by printing the message, this now means we must commit the offset
                     // to assure exactly once semantics in case of failure.
                     // We commit the offset that is coming up as that is the point we would want to start from
-                    myProcessedOffsets.put(new TopicPartition(record.topic(),record.partition()),new OffsetAndMetadata(record.offset()+1,null));
+                    if (myProcessedOffsets.containsKey(new TopicPartition(record.topic(),record.partition()))) {
+                        myProcessedOffsets.put(new TopicPartition(record.topic(),record.partition()),new OffsetAndMetadata(record.offset()+1,null));
+                    }
                     // We provide a callback in case of failure
                     consumer.commitAsync(myProcessedOffsets, (offsets, exception) -> {
-                        // We have no tolerance for failed consumer offset commits
-                        throw new KafkaException("Offset Commit was unsuccessful");
+                        // We have no tolerance for failed consumer offset commits that aren't retry-able
+                        if (exception instanceof CommitFailedException) {
+                            throw new KafkaException("Offset Commit was unsuccessful, this is nonrecoverable");
+                        } else if (exception instanceof InterruptException){
+                            throw new KafkaException("Thread was interrupted");
+                        } else if (exception instanceof AuthorizationException) {
+                            throw new KafkaException("Not authorized to commit offsets");
+                        } else if ( exception == null ) {
+                            logging.debug("Successfully committed offsets");
+                        }
+                        else {
+                            logging.warn("Recoverable Exception, will rely on the next offset commit cycle");
+                        }
                     });
                 }
             }
@@ -77,7 +112,6 @@ public class commonConsumerManualOffsetCommit {
         catch (Exception e){
             e.printStackTrace();
         }
-
         //Shutdown hook to assure final processing and graceful shutdown when SIGTERM is received
         Runtime.getRuntime().addShutdownHook(new Thread(consumer::close));
     }
